3f6cdd87c23517d4f73845f44f77fff140db51e5
Rick Bacci <rickbacci@gmail.com>
2016-01-06 19:08:06 -0700

    update loan_requests#index and view

--- app/controllers/loan_requests_controller.rb
+++ app/controllers/loan_requests_controller.rb
@@ -2,7 +2,6 @@ class LoanRequestsController < ApplicationController
   before_action :set_loan_request, only: [:update, :show]
 
   def index
-    @loan_requests = LoanRequest.all
     @loan_requests = LoanRequest.page params[:page]
   end
 
